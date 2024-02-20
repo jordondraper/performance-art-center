013304da1ab89d1ed38c1f977900783939185d9b
Jordon Draper <jordondraper17@gmail.com>
2024-02-19 20:10:45 -0700

    upload for now

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+
+const x = document.getElementById("funButton");
+
+x.onclick = toggleMenu;
+
+
+
 // inject current year in footer
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
